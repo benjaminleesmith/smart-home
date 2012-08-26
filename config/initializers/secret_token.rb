b85f46916da6b19cb7b8bcb4677eecb128995a14
Benjamin Smith <benjamin.lee.smith@gmail.com>
2012-08-25 18:11:35 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartHome::Application.config.secret_token = '55aa290458e4b12598b54695f4608ba4ca9b30e8679bf124cceda52730e997055dbe4b9936c23927e57c46d76a93d546c09b5be0ffba8c84ca6fce5860e04ea0'
